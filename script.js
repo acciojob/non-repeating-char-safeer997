7784a4783f6f270ff7e0092997fc41f6d6516692
safeer997 <143935408+safeer997@users.noreply.github.com>
2024-12-27 15:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let uniqueCount = 1 ;
+	for(let i= 0 ; i<str.length ; i++){
+		for(let j = 0 ; j<str.length ; j++){
+			if(str[i]==str[j]){
+				if(i==j){
+					continue ;
+				}
+				uniqueCount++
+			}
+		}
+	}
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
